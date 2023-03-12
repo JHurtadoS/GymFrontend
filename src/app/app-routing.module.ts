663ccef7b03c91070c0c09f519f205a4576ed16c
karen tovar <karenttb23@gmail.com>
2023-03-12 18:17:44 -0500

    Conexion del back con el Fron

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AsistenciaComponent } from './Components/asistencia/asistencia.component';
+import { EjerciciosComponent } from './Components/ejercicios/ejercicios.component';
+import { EventoComponent } from './Components/evento/evento.component';
+import { FormularioComponent } from './Components/formulario/formulario.component';
+import { HerramientaComponent } from './Components/herramienta/herramienta.component';
+import { LoginComponent } from './Components/login/login.component';
+import { PersonaComponent } from './Components/persona/persona.component';
+import { RutinaComponent } from './Components/rutina/rutina.component';
+import { UsuarioComponent } from './Components/usuario/usuario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{path:"Persona", component:PersonaComponent},
+{path:"Asistencia", component:AsistenciaComponent},
+{path:"Ejercicios", component:EjerciciosComponent},
+{path:"Evento", component:EventoComponent},
+{path:"Formulario", component:FormularioComponent},
+{path:"Herramienta", component:HerramientaComponent},
+{path:"login", component:LoginComponent},
+{path:"Rutina", component:RutinaComponent},
+{path:"Usuario", component:UsuarioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,64 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './Components/formulario/formulario.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MenuComponent } from './Components/menu/menu.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { AsistenciaComponent } from './Components/asistencia/asistencia.component';
+import { EjerciciosComponent } from './Components/ejercicios/ejercicios.component';
+import { EventoComponent } from './Components/evento/evento.component';
+import { PersonaComponent } from './Components/persona/persona.component';
+import { UsuarioComponent } from './Components/usuario/usuario.component';
+import { LoginComponent } from './Components/login/login.component';
+import { HerramientaComponent } from './Components/herramienta/herramienta.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent,
+    HerramientaComponent,
+    AsistenciaComponent,
+    EjerciciosComponent,
+    EventoComponent,
+    PersonaComponent,
+    UsuarioComponent,
+    LoginComponent,
+    MenuComponent
+
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
